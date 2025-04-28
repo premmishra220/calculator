0e95a991bb304aefd3c2e229feb62657f3728c35
premmishra220 <premmishrasonb@gmail.com>
2025-04-28 19:27:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  if (display.innerText === "0") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  display.innerText = display.innerText.slice(0, -1) || "0";
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText.replace('÷', '/').replace('×', '*'));
+  } catch {
+    display.innerText = "Error";
+  }
+}
